c5f27882b56b2dda6dc5a401468957a8290f5f8a
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2018-02-22 18:13:26 +0100

    loops warmup

---
+++ src/com/gulci/java/sda/LoopsWarmup.java
@@ -0,0 +1,74 @@
+package com.gulci.java.sda;
+
+import java.util.Scanner;
+
+public class LoopsWarmup {
+
+    public static void main(String[] args) {
+
+        // .var - creates variable from expression
+
+        Scanner reader = new Scanner(System.in);
+        int n;
+
+        // 1
+//        System.out.print("Choose a number for division check: ");
+//        n = reader.nextInt();
+
+//        for (int i = 1; i <= n; i++) {
+//            System.out.print("Pick number " + i + ": ");
+//            int ith = reader.nextInt();
+//
+//            if (ith % 2 == 0) {
+//                System.out.println("Number " + ith + " is divisible by 2");
+//            }
+//
+//            if (ith % 3 == 0) {
+//                System.out.println("Number " + ith + " is divisible by 3");
+//            }
+//
+//            if (ith % 6 == 0) {
+//                System.out.println("Number " + ith + " is divisible by 6");
+//            }
+//        }
+
+        // 2
+//        System.out.print("Choose a number: ");
+//        n = reader.nextInt();
+//        int last = 0;
+//
+//        for (int i = 1; i <= n; i++) {
+//            System.out.print("Pick number " + i + ": ");
+//            int ith = reader.nextInt();
+//            System.out.println("Last number was " + last);
+//            last = ith;
+//        }
+//
+//        System.out.println("The very last number was " + last);
+
+        // 3
+//        System.out.print("Choose a number for sum of even: ");
+//        n = reader.nextInt();
+//        int total = 0;
+//
+//        for (int i = 0; i < n; i++) {
+//            if (i % 2 == 0) {
+//                total += i;
+//            }
+//        }
+//
+//        System.out.println("Total is: " + total);
+
+        // 4
+//        System.out.print("Choose a number for factorial: ");
+//        n = reader.nextInt();
+//        int factorial = 1;
+//
+//        for (int i = n; i > 1; i--) {
+//            factorial *= i;
+//        }
+//
+//        System.out.println("Factorial of " + n + " is: " + factorial);
+    }
+
+}
